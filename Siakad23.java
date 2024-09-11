a9eb0e41ad496fdd8d6dfd964b41e55a2a290487
qulbyazz <qulbyazzumy@gmail.com>
2024-09-11 23:07:31 +0700

    commit ke 2

--- Siakad23.java
+++ Siakad23.java
@@ -5,30 +5,33 @@ public class Siakad23{
         Scanner input = new Scanner(System.in);
 
         String nama,nim;
-        char Kelas;
+        String Kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        int nilaiKuis, nilaiTugas, nilaiUjian, nilaiUAS, nilaiAkhir;
 
         System.out.print("Masukkan Nama: ");
         nama = input.nextLine();
         System.out.print("Masukkan NIM: ");
         nim = input.nextLine();
         System.out.print("Masukkan Kelas: ");
-        Kelas = input.nextLine().charAt(0);
+        Kelas = input.nextLine();
         System.out.print("Masukkan Nomor Absen: ");
         absen = input.nextByte();
 
         System.out.println("-----Masukkan Komponen Nilai-----");
         System.out.print("Nilai Kuis: ");
-        nilaiKuis = input.nextDouble();
+        nilaiKuis = input.nextInt();
         System.out.print("Nilai Tugas: ");
-        nilaiTugas = input.nextDouble();
+        nilaiTugas = input.nextInt();
         System.out.print("Nilai Ujian: ");
-        nilaiUjian = input.nextDouble();
+        nilaiUjian = input.nextInt();
+        System.out.print("Nilai UAS: ");
+        nilaiUAS = input.nextInt();
         
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiAkhir = (int) ((nilaiKuis*0.2) + (nilaiTugas*0.15) + (nilaiUjian*0.3) + (nilaiUAS*0.35));
+        double totalnilaiakhir = nilaiAkhir / 4;
 
-        System.out.println("Mahasiswa dengan nama " + nama + "NIM " + nim + "Kelas " + Kelas + "Absen " + absen);
-        System.out.println("Nilai Akhir: " +nilaiAkhir);
+        System.out.println("Mahasiswa dengan nama " + nama + " NIM " + nim + " Kelas " + Kelas + " Absen " + absen);
+        System.out.println("Nilai Akhir: " +totalnilaiakhir);
     }
 }
